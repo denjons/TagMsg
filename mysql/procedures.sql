e8408de2571d23e3bce3e571765130fa59222ad2
denjons <dennis.j@live.se>
2017-03-08 00:25:55 +0100

    mysql update to response

--- mysql/procedures.sql
+++ mysql/procedures.sql
@@ -1,19 +1,4 @@
 
-/*
-	adds user and returns new id 
-    or 
-    returns id for existing user
-*/
-DELIMITER //
-create procedure CREATE_USER()
-BEGIN
-	set @uuid = UUID();
-	INSERT INTO USERS (uu_id, date) VALUES (@uuid, current_timestamp());
-	SELECT * FROM Users WHERE Users.id = @uuid;
-end //
-DELIMITER ;
-
-
 
 /*
 		***************************** TAGS ********************************
@@ -61,22 +46,6 @@ DELIMITER ;
 /*
 		************************ REQUESTS *************************
 */
-
-/*
-	add request
-*/
-DELIMITER //
-create procedure ADD_REQUEST(user_id integer, content_input TEXT, req_id VARCHAR(40))
-begin
-    
-	if (REQUEST_EXISTS(req_id) = -1) then
-		insert into REQUESTS (uu_id, user, content) values (req_id, user_id, content_input) ;
-    else
-		signal sqlstate '20005';
-    end if ;
-    
-end//
-DELIMITER ;
 
 
 /*
@@ -126,6 +95,9 @@ DELIMITER ;
 /*
 		get all requests a user is eligible for
 */
+
+-- drop procedure GET_ELIGIBLE_REQUESTS;
+
 DELIMITER //
 create procedure GET_ELIGIBLE_REQUESTS(userId integer, requestLimit int, requestOffset int, fromRequest VARCHAR(40), beforeRequest VARCHAR(40))
 begin
@@ -141,7 +113,7 @@ begin
         AND (@endDate >= REQUESTS_VIEW.date OR @endDate IS NULL)
         order by date DESC limit requestLimit offset requestOffset;
     else
-		select table1.request as id, REQUESTS_VIEW.user, REQUESTS_VIEW.content, REQUESTS_VIEW.tags as tags , REQUESTS_VIEW.date from
+		select table1.request as id, REQUESTS_VIEW.uu_id, REQUESTS_VIEW.user, REQUESTS_VIEW.content, REQUESTS_VIEW.tags as tags , REQUESTS_VIEW.date from
 		(select request from ELIGIBLE_VIEW where ELIGIBLE_VIEW.user = userId ) as table1
 		join 
 		REQUESTS_VIEW 
@@ -159,69 +131,45 @@ DELIMITER ;
 		get requests for tags
 */
 
+-- drop procedure GET_REQUESTS_FROM_TAGS;
 
 DELIMITER //
-create procedure GET_REQUESTS_FROM_TAGS(tags TEXT, requestLimit int, requestOffset int, startDate TimeStamp, endDate Timestamp)
+create procedure GET_REQUESTS_FROM_TAGS(tags TEXT, requestLimit int, requestOffset int, startDate  VARCHAR(40))
 begin
 	
     SET @tags = tags;
     SET @limit = requestLimit;
     SET @offset = requestOffset;
-    SET @startDate = startDate;
-    SET @endDate = endDate;
-    
+	SET @startDate = startDate;  
+
     SET @Query = concat(
-    'select REQUESTS_VIEW.id, REQUESTS_VIEW.user, REQUESTS_VIEW.content, REQUESTS_VIEW.date, REQUESTS_VIEW.tags',
-    ' from OfTag join REQUESTS_VIEW on',
-    ' OfTag.tag in (?)',
-    ' AND (? <= REQUESTS_VIEW.date OR ? IS NULL)',
-    ' AND (? >= REQUESTS_VIEW.date OR ? IS NULL)',
-    ' AND REQUESTS_VIEW.id = OfTag.request',
+    'select REQUESTS_VIEW.id, REQUESTS_VIEW.user, REQUESTS_VIEW.uu_id, REQUESTS_VIEW.content, REQUESTS_VIEW.date, REQUESTS_VIEW.tags',
+    ' from REQUESTS_TAG_RELATION join REQUESTS_VIEW on',
+    ' REQUESTS_TAG_RELATION.tag in (?)',
+   -- ' AND (? IS NULL OR ? <= REQUESTS_VIEW.date)',
+    ' AND REQUESTS_VIEW.id = REQUESTS_TAG_RELATION.request',
     ' limit ? offset ?;');
     
     PREPARE stmt FROM @query;
 	EXECUTE stmt using 
-    @tags, @startDate, @startDate, @endDate, @endDate, @limit, @offset;
+    @tags, /* @startDate, @startDate, */ @limit, @offset;
     
 end //
 DELIMITER ;
 
-
-/*
-		add response for request
-        updates if already exists
-*/
-DELIMITER //
-create procedure ADD_RESPONSE_TO_REQUEST(user_id integer, resp_id varchar(40), resp_content TEXT, req_id varchar(40))
-begin
-	/* if user does not exist*/
-	declare req_db_id integer;
-
-	set req_db_id = REQUEST_EXISTS(req_id);
-    
-	if (RESPONSE_EXISTS(resp_id) = -1) then
-		insert into RESPONSES (id, user, request, content) values (resp_id, responder, req_id, resp_content) ;
-    else
-		/* upadte response */
-        update RESPONSES set content = resp_content where RESPONSES.id = req_db_id AND RESPONSES.user = user_id;
-    end if;
-
-end //
-DELIMITER ;
 
 /*
 		get responses for request
 */
+-- drop procedure GET_RESPONSES_FOR_REQUESTS;
 DELIMITER //
-create procedure GET_RESPONSES_FOR_REQUESTS( reqId varchar(40), responseLimit int, responseOffset int, fromResponse VARCHAR(40), beforeResponse VARCHAR(40))
+create procedure GET_RESPONSES_FOR_REQUESTS( reqId integer, responseLimit int, responseOffset int, afterResponse VARCHAR(40), beforeResponse VARCHAR(40))
 begin 
 
-	declare req_db_id int;
-	set @startDate = GET_RESPONSE_DATE(fromResponse);
+	set @startDate = GET_RESPONSE_DATE(afterResponse);
     set @endDate = GET_RESPONSE_DATE(beforeResponse);
-    set req_db_id = REQUEST_EXISTS(reqId);
     
-	select * from RESPONSES where  RESPONSES.request = req_db_id
+	select * from RESPONSES where  RESPONSES.request = reqId
     AND (@startDate <= RESPONSES.date OR @startDate IS NULL)
 	AND (@endDate >= RESPONSES.date OR @endDate IS NULL)
 	order by date DESC limit responseLimit offset responseOffset;
--- mysql/test23.sql
+++ mysql/test23.sql
@@ -1,8 +1,8 @@
 delete from USER_TAG_RELATION;
 delete from requests_tag_relation;
 delete from tags;
-delete from requests;
 delete from responses;
+delete from requests;
 delete from users;
 
 insert into users (date) values(current_timestamp());
@@ -11,14 +11,10 @@ insert into users (date) values(current_timestamp());
 select max(id) into @id2 from users;
 
 
-call ADD_USER_TAG(@id1, 'tag1');
-call ADD_USER_TAG(@id1, 'tag3');
-call ADD_USER_TAG(@id2, 'tag1');
+call ADD_USER_TAG(@id1, 'english');
+call ADD_USER_TAG(@id1, 'japanese');
+call ADD_USER_TAG(@id2, 'english');
 
-
-set @req1 = UUID();
-set @req2 = UUID();
-set @req3 = UUID();
 
 call ADD_REQUEST(@id2, 'req 1', UUID());
 select max(id) into @req1 from `REQUESTS`;
@@ -27,11 +23,14 @@ select max(id) into @req2 from `REQUESTS`;
 call ADD_REQUEST(@id2, 'req 3', UUID());
 select max(id) into @req3 from `REQUESTS`;
 
-call ADD_REQUEST_TAG(@req1, 'tag1');
-call ADD_REQUEST_TAG(@req1, 'tag2');
-call ADD_REQUEST_TAG(@req2, 'tag1');
-call ADD_REQUEST_TAG(@req3, 'tag1');
+call ADD_REQUEST_TAG(@req1, 'english');
+call ADD_REQUEST_TAG(@req1, 'german');
+call ADD_REQUEST_TAG(@req2, 'english');
+call ADD_REQUEST_TAG(@req3, 'english');
+
+insert into Responses (uu_id, user, content, request) values('12121212-12121212-12121212-12121212', @id1, 'va saaa ruu?', @req1);
+insert into Responses (uu_id, user, content, request) values('42121212-12121212-12121212-12121212', @id1, 'nante iwamasuka?', @req1);
+
+call GET_RESPONSES_FOR_REQUESTS(@req1, 10, 0, null, null);
 
 
-call `GET_ELIGIBLE_REQUESTS`(@id1, 5, 0, null, null);
-call `GET_ELIGIBLE_REQUESTS`(@id2, 5, 0, null, null);
